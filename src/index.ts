44ce81d6184eb6adab9e353b5146d2fe8a36a9d5
CHANG Xuben <changxuben@gmail.com>
2025-03-27 01:07:19 +0800

    docs: add command help

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Context, Schema } from 'koishi'
+import { type Context, Schema } from 'koishi'
 import * as z from 'zero-width-lib'
 
 export const name = 'zero-width'
@@ -14,13 +14,9 @@ export const Config: Schema<Config> = Schema.object({
 export function apply(ctx: Context, config: Config) {
   const globalTimeout = config.timeout
   
-  ctx.command('zero').alias('零宽')
-    .usage('零宽字符工具\n' +
-        'zero embed(emb/嵌入) [可见文本] [隐藏文本] - 将隐藏文本以零宽字符嵌入可见文本中\n' +
-        'zero extract(ext/提取) [文本] - 提取指定文本中的零宽字符并解码\n' +
-        'zero escape(esc/转义) [文本] - 使用零宽字符分隔文本来规避搜索')
+  ctx.command('zero', '零宽字符工具').alias('零宽')
   
-  ctx.command('zero.embed [visible] [hidden]')
+  ctx.command('zero.embed [visible] [hidden]', '将隐藏文本以零宽字符嵌入可见文本中')
     .alias('zero.emb').alias('zero.嵌入')
     .action(async ({ session }, visible, hidden) => {
       let finalVisible = visible
@@ -50,7 +46,7 @@ export function apply(ctx: Context, config: Config) {
       }
     })
 
-  ctx.command('zero.extract [text:text]')
+  ctx.command('zero.extract [text:text]', '提取指定文本中的零宽字符并解码')
     .alias('zero.ext').alias('zero.提取')
     .action(async ({ session }, text) => {
       let finalText = text
@@ -73,7 +69,7 @@ export function apply(ctx: Context, config: Config) {
       }
     })
 
-  ctx.command('zero.escape [text]')
+  ctx.command('zero.escape [text]', '使用零宽字符分隔文本来规避搜索')
     .alias('zero.esc').alias('zero.转义')
     .action(async ({ session }, text) => {
       let finalText = text
